ecae769f97c41dde9e69bb1a84a08f59d795b37a
ry_Tom <ryotoyota@outlook.jp>
2024-10-07 21:21:17 +0900

    code for build site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: process.env.NODE_ENV == 'development' ? undefined : 'export',
+};
 
 export default nextConfig;
