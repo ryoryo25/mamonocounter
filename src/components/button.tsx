56709ee51797e0d8eb8784013d8e74e45746ac1e
ry_Tom <ryotoyota@outlook.jp>
2024-10-12 20:58:22 +0900

    nice button design

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,10 @@ import { ButtonHTMLAttributes } from "react"
 
 const Button = ({ className, children, ...props}: ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button className={`border-neutral-500 border-2 border-spacing-2 rounded ${className}`} {...props} >
+    <button
+      className={`bg-neutral-50 active:bg-neutral-200 border-neutral-500 border-2 rounded-full px-4 py-2 ${className}`}
+      {...props}
+    >
       {children}
     </button>
   )
