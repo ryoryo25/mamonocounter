0bc7af58019c788a85098a03cf0fb376cc140e70
ry_Tom <ryotoyota@outlook.jp>
2024-10-19 01:35:42 +0900

    flatten live list and sort by date

--- src/components/live-list.tsx
+++ src/components/live-list.tsx
@@ -7,38 +7,61 @@ type Props = {
   onChange: ChangeEventHandler<HTMLInputElement>
 }
 
+interface FlattenedLive extends LiveDetail {
+  title: string
+}
+
 const getDateString = (date: string): string => {
   return date.replaceAll("-", "/")
 }
 
-const getLabel = (live: Live, stage: LiveDetail, i: number): string => {
-  const no = stage.no || (live.details.length > 1 ? "Day" + (i + 1) : "")
-  const info = stage.info ? ` (${stage.info})` : ""
-  const date = getDateString(stage.date)
+const getLabel = (live: FlattenedLive): string => {
+  const no = live.no || ""
+  const info = live.info ? ` (${live.info})` : ""
+  const date = getDateString(live.date)
   return `${live.title} ${no}${info} (${date})`
 }
 
-const generateCheckbox = (live: Live, onChange: ChangeEventHandler) => {
-  return live.details.map((stage, i) => {
-    const label = getLabel(live, stage, i)
-    return (
-      <label key={label} className={`py-2 ${stage.disabled ? "text-neutral-400" : ""}`}>
-        <input
-          type="checkbox"
-          value={Number(stage.mamono)}
-          className="mr-2"
-          onChange={onChange}
-          disabled={stage.disabled}
-        />
-        {label}
-      </label>
-    )})
+const generateCheckbox = (live: FlattenedLive, onChange: ChangeEventHandler) => {
+  const label = getLabel(live)
+  return (
+    <label key={label} className={`py-2 ${live.disabled ? "text-neutral-400" : ""}`}>
+      <input
+        type="checkbox"
+        value={Number(live.mamono)}
+        className="mr-2"
+        onChange={onChange}
+        disabled={live.disabled}
+      />
+      {label}
+    </label>
+  )
+}
+
+const flattenLives = (lives: Live[]): FlattenedLive[] => {
+  return lives.flatMap(live => {
+    return live.details.map((stage, i) => {
+      if (live.details.length > 1 && !stage.no) {
+        stage.no = `Day${i+1}`
+      }
+      return { title: live.title, ...stage }
+    })
+  }).sort((a, b) => {
+    if (a.date > b.date) {
+      return 1
+    } else if (a.date < b.date) {
+      return -1
+    } else {
+      return 0
+    }
+  })
 }
 
 const LiveList = ({ lives, onChange }: Props) => {
+  const flattenedLives = flattenLives(lives)
   return (
     <div className="flex flex-col mb-8">
-      {lives.map(live => generateCheckbox(live, onChange))}
+      {flattenedLives.map(live => generateCheckbox(live, onChange))}
     </div>
   )
 }
